b8a333a47df255a28d263a50cb6c3602f00f4bb5
Surendra Pratap Sing <sps783804@gmail.com>
2024-04-08 20:58:06 +0530

    commit

--- hibernate_project/src/main/java/com/sp/hibernate/Main.java
+++ hibernate_project/src/main/java/com/sp/hibernate/Main.java
@@ -7,26 +7,26 @@ import org.hibernate.cfg.Configuration;
 import com.sp.entity.Student;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		Configuration config=new Configuration();
+
+		Configuration config = new Configuration();
 		config.configure("hibernate.cfg.xml");
-		
-		SessionFactory sessionFactory=config.buildSessionFactory();
-		Session session=sessionFactory.openSession();
-		
-		Transaction tx=  session.beginTransaction();
-		Student student=new Student();
-		
+
+		SessionFactory sessionFactory = config.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
+		Transaction tx = session.beginTransaction();
+		Student student = new Student();
+
+		//create
 		student.setId(103);
 		student.setName("sp");
-		
-		
+
 		session.save(student);
-		
+
 		tx.commit();
-		
+
 		session.close();
 	}
 
